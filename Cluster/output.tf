155a6a97b818c09b8e40879d272b3b0c5d53cad5
jayapaul771 <jayapoulpilli@gmail.com>
2025-07-09 22:33:49 +0530

    Update output.tf

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.yogesh.id
+  value = aws_eks_cluster.paul.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.yogesh.id
+  value = aws_eks_node_group.paul.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.yogesh_vpc.id
+  value = aws_vpc.paul_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.yogesh_subnet[*].id
+  value = aws_subnet.paul_subnet[*].id
 }
 
