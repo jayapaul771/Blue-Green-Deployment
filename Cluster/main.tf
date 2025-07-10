0f9c1c5e1a25bc9b4cfde1d68ea87bd214c29256
jayapaul771 <jayapoulpilli@gmail.com>
2025-07-10 15:43:43 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -1,56 +1,56 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-northeast-2"
 }
 
-resource "aws_vpc" "paul_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "mammu_vpc" {
+  cidr_block = "18.0.0.0/16"
 
   tags = {
-    Name = "paul-vpc"
+    Name = "mammu-vpc"
   }
 }
 
-resource "aws_subnet" "paul_subnet" {
+resource "aws_subnet" "mammu_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.paul_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.paul_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  vpc_id                  = aws_vpc.mammu_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.mammu_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["ap-northeast-2a", "ap-northeast-2b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "paul-subnet-${count.index}"
+    Name = "mammu-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "paul_igw" {
-  vpc_id = aws_vpc.paul_vpc.id
+resource "aws_internet_gateway" "mammu_igw" {
+  vpc_id = aws_vpc.mammu_vpc.id
 
   tags = {
-    Name = "paul-igw"
+    Name = "mammu-igw"
   }
 }
 
-resource "aws_route_table" "paul_route_table" {
-  vpc_id = aws_vpc.paul_vpc.id
+resource "aws_route_table" "mammu_route_table" {
+  vpc_id = aws_vpc.mammu_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.paul_igw.id
+    gateway_id = aws_internet_gateway.mammu_igw.id
   }
 
   tags = {
-    Name = "paul-route-table"
+    Name = "mammu-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.paul_subnet[count.index].id
-  route_table_id = aws_route_table.paul_route_table.id
+  subnet_id      = aws_subnet.mammu_subnet[count.index].id
+  route_table_id = aws_route_table.mammu_route_table.id
 }
 
-resource "aws_security_group" "paul_cluster_sg" {
-  vpc_id = aws_vpc.paul_vpc.id
+resource "aws_security_group" "mammu_cluster_sg" {
+  vpc_id = aws_vpc.mammu_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "paul_cluster_sg" {
   }
 
   tags = {
-    Name = "paul-cluster-sg"
+    Name = "mammu-cluster-sg"
   }
 }
 
-resource "aws_security_group" "paul_node_sg" {
-  vpc_id = aws_vpc.paul_vpc.id
+resource "aws_security_group" "mammu_node_sg" {
+  vpc_id = aws_vpc.mammu_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,25 +82,25 @@ resource "aws_security_group" "paul_node_sg" {
   }
 
   tags = {
-    Name = "paul-node-sg"
+    Name = "mammu-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "paul" {
-  name     = "paul-cluster"
-  role_arn = aws_iam_role.paul_cluster_role.arn
+resource "aws_eks_cluster" "mammu" {
+  name     = "mammu-cluster"
+  role_arn = aws_iam_role.mammu_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.paul_subnet[*].id
-    security_group_ids = [aws_security_group.paul_cluster_sg.id]
+    subnet_ids         = aws_subnet.mammu_subnet[*].id
+    security_group_ids = [aws_security_group.mammu_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "paul" {
-  cluster_name    = aws_eks_cluster.paul.name
-  node_group_name = "paul-node-group"
-  node_role_arn   = aws_iam_role.paul_node_group_role.arn
-  subnet_ids      = aws_subnet.paul_subnet[*].id
+resource "aws_eks_node_group" "mammu" {
+  cluster_name    = aws_eks_cluster.mammu.name
+  node_group_name = "mammu-node-group"
+  node_role_arn   = aws_iam_role.mammu_node_group_role.arn
+  subnet_ids      = aws_subnet.mammu_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -112,12 +112,12 @@ resource "aws_eks_node_group" "paul" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.paul_node_sg.id]
+    source_security_group_ids = [aws_security_group.mammu_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "paul_cluster_role" {
-  name = "paul-cluster-role"
+resource "aws_iam_role" "mammu_cluster_role" {
+  name = "mammu-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +135,13 @@ resource "aws_iam_role" "paul_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "paul_cluster_role_policy" {
-  role       = aws_iam_role.paul_cluster_role.name
+resource "aws_iam_role_policy_attachment" "mammu_cluster_role_policy" {
+  role       = aws_iam_role.mammu_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "paul_node_group_role" {
-  name = "paul-node-group-role"
+resource "aws_iam_role" "mammu_node_group_role" {
+  name = "mammu-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +159,17 @@ resource "aws_iam_role" "paul_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "paul_node_group_role_policy" {
-  role       = aws_iam_role.paul_node_group_role.name
+resource "aws_iam_role_policy_attachment" "mammu_node_group_role_policy" {
+  role       = aws_iam_role.mammu_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "paul_node_group_cni_policy" {
-  role       = aws_iam_role.paul_node_group_role.name
+resource "aws_iam_role_policy_attachment" "mammu_node_group_cni_policy" {
+  role       = aws_iam_role.mammu_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "paul_node_group_registry_policy" {
-  role       = aws_iam_role.paul_node_group_role.name
+resource "aws_iam_role_policy_attachment" "mammu_node_group_registry_policy" {
+  role       = aws_iam_role.mammu_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
