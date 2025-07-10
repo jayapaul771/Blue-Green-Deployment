0e648bba54805c1e5c6b10e4320034292f670341
jayapaul771 <jayapoulpilli@gmail.com>
2025-07-10 15:44:23 +0530

    Update output.tf

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.paul.id
+  value = aws_eks_cluster.mammu.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.paul.id
+  value = aws_eks_node_group.mammu.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.paul_vpc.id
+  value = aws_vpc.mammu_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.paul_subnet[*].id
+  value = aws_subnet.mammu_subnet[*].id
 }
 
